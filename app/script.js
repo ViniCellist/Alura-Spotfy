b1215f24fcc2c319baa5d44a620dae336b7f14a5
Vinicius Duarte <vsduartestudy@outlook.com>
2024-01-25 15:49:06 -0300

    feat: api request

---
+++ app/script.js
@@ -0,0 +1,33 @@
+const searchInput = document.querySelector('#search-input');
+const resultArtist = document.querySelector('#result-artist');
+const resultPlaylist = document.querySelector('#result-playlist');
+
+function requestAPI(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
+
+function displayResults(result) {
+    resultPlaylist.classList.add('hidden');
+    const artistName = document.querySelector('#artist-name');
+    const artistImage = document.querySelector('#artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
+}
+
+searchInput.addEventListener('input', () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return
+    }
+    requestAPI(searchTerm);
+})
